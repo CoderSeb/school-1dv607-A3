f5feff9e71261c7f37e69451a7c866e79619645f
Sebastian <sa224ny@student.lnu.se>
2021-10-21 15:02:27 +0200

    Implemented call for giveHidden-/giveOpenCard methods on dealer.

--- src/main/java/model/rules/AmericanNewGameStrategy.java
+++ src/main/java/model/rules/AmericanNewGameStrategy.java
@@ -1,6 +1,5 @@
 package model.rules;
 
-import model.Card;
 import model.Dealer;
 import model.Deck;
 import model.Player;
@@ -8,23 +7,10 @@ import model.Player;
 class AmericanNewGameStrategy implements NewGameStrategy {
 
   public boolean newGame(Deck deck, Dealer dealer, Player player) {
-    Card.Mutable c;
-
-    c = deck.getCard();
-    c.show(true);
-    player.dealCard(c);
-
-    c = deck.getCard();
-    c.show(true);
-    dealer.dealCard(c);
-
-    c = deck.getCard();
-    c.show(true);
-    player.dealCard(c);
-
-    c = deck.getCard();
-    c.show(false);
-    dealer.dealCard(c);
+    dealer.giveOpenCard(player);
+    dealer.giveOpenCard(dealer);
+    dealer.giveOpenCard(player);
+    dealer.giveHiddenCard(dealer);
 
     return true;
   }
