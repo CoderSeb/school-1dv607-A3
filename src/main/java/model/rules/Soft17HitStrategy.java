53deb3e3c90cdf0a3662baee695e55e1d1880070
Sebastian <sa224ny@student.lnu.se>
2021-10-21 14:59:39 +0200

    Added check for more aces in isSoft method

--- src/main/java/model/rules/Soft17HitStrategy.java
+++ src/main/java/model/rules/Soft17HitStrategy.java
@@ -22,12 +22,15 @@ public class Soft17HitStrategy implements HitStrategy {
     int[] cardScores = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11 };
     int score = 0;
     int firstScore = dealer.calcScore();
-
+    int nrOfAces = 0;
     for (Card c : dealer.getHand()) {
       if (c.getValue() != Card.Value.Ace) {
         score += cardScores[c.getValue().ordinal()];
+      } else {
+        nrOfAces += 1;
       }
     }
-    return score != (firstScore - 1);
+
+    return score != (firstScore - nrOfAces);
   }
 }
